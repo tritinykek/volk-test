1d618586f725f50194839d56c57798d070018402
tritiny <58145716+tritinykek@users.noreply.github.com>
2020-09-05 16:07:56 +0300

    New update 228337 for u

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.button)
+        val textView: TextView = findViewById(R.id.textView)
+        var i =0
+        button.setOnClickListener(){
+            i+=1
+            textView.text = "На земле есть " + i.toString() + " лохов"
+
+        }
     }
+
 }
